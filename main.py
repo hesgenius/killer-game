b82adddf886788108ed1a4fdc639c60182aa0cfc
hesgenius <83162120+hesgenius@users.noreply.github.com>
2022-08-24 12:51:14 +0300

    add token

--- main.py
+++ main.py
@@ -1,7 +1,1 @@
-import telebot, json, os
-from telebot import types
-
-try:
-    token = "5611629145:AAFDdxvmIdRWuXl0c8fB0sNaWlR98yVHQco"
-    bot = telebot.TeleBot(token)
-    @bot.message_handler=(commandsa=['/start'])
+token = "5611629145:AAFDdxvmIdRWuXl0c8fB0sNaWlR98yVHQco"
