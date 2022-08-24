97cebf0c04c20c70302d51c25e98a92b8c3545e3
hesgenius <83162120+hesgenius@users.noreply.github.com>
2022-08-24 12:34:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+import telebot, json, os
+from telebot import types
 
+try:
+    token = "5611629145:AAFDdxvmIdRWuXl0c8fB0sNaWlR98yVHQco"
+    bot = telebot.TeleBot(token)
+    @bot.message_handler=(commandsa=['/start'])
